af862ea47e96c73557795747c0d3b8f71c06296d
Andrew Dutcher <andrew@andrewdutcher.com>
2013-12-08 13:47:57 -0800

    Cleanup for github

---
+++ multicheck.py
@@ -0,0 +1,4 @@
+def main(a):
+	return len(a.split('<html>') > 2)
+
+descripion = 'invalid html'
--- tcpread.py
+++ tcpread.py
@@ -3,12 +3,13 @@
 import datetime, sys, string, re
 
 class Packet:
-	def __init__(self, debug):
+	def __init__(self, config, fmtdat, debug):
 		self.clear()
+		self.config = config
+		self.fmtdat = fmtdat
 		self.debug = debug
 	
 	def add_line(self, line):
-		global config
 		self.raw_lines.append(line)
 		if self.debug:
 			print line
@@ -18,8 +19,7 @@ class Packet:
 				return
 			self.state = 1
 			linecomp = line.split(' ')
-			times = map(int, linecomp[0].split('.')[0].split(':'))
-			self.time = datetime.time(hour=times[0], minute=times[1], second=times[2])
+			self.time = linecomp[0]
 			self.datalength = getlen(line)
 			baseproto = linecomp[1] if not ':' in linecomp[1] else linecomp[2]
 			if baseproto == 'IP':
@@ -37,9 +37,9 @@ class Packet:
 			noport = len(destid) < 5
 			if not noport:
 				#MEET THE FILTER FROM HELL
-				tflist = map(lambda x: x == '*' or x == sourceid[4] or x == destid[4], config[0])
-				self.matches = filter(lambda x: tflist[x[0]], enumerate(config[1]))
-			self.ident = ('.'.join(sourceid[:4]), 0 if noport else int(sourceid[4]), '.'.join(destid[:4]), 0 if noport else int(destid[4]), self.time)
+				tflist = map(lambda x: x == '*' or x == sourceid[4] or x == destid[4], self.config[0])
+				self.matches = filter(lambda x: tflist[x[0]], enumerate(self.config[1]))
+			self.ident = ('.'.join(sourceid[:4]), 0 if noport else int(sourceid[4]), '.'.join(destid[:4]), 0 if noport else int(destid[4]))
 			if 'length' in line:
 				self.streamlength = getlen(line)
 		elif self.state == 2:
@@ -70,7 +70,6 @@ class Packet:
 
 
 	def parse(self):
-		global config
 		if self.debug:
 			print 'finished packet:'
 			print self.streamdata
@@ -79,13 +78,25 @@ class Packet:
 			print 'Checking against',map(lambda x: x[1].pattern, self.matches)
 		for fil in self.matches:
 			if fil[1].search(self.streamdata):
-				print '---------------------- Match Found ----------------------'
-				print 'Rule ' + str(fil[0]) + ', port ' + config[0][fil[0]] + ', ' + fil[1].pattern
-				print self.ident
-				print '\n'.join(self.raw_lines)
-				print '\nApplication-layer data:'
-				print self.appdata
-				print '----------------------- Match End -----------------------'
+				if self.fmtdat['header']:
+					print '---------------------- Match Found ----------------------'
+				for c in self.fmtdat['format']:
+					if c == 'r':
+						print 'Rule ' + str(fil[0]) + ', port ' + self.config[0][fil[0]] + ', ' + fil[1].pattern
+					if c == 'i':
+						print 'From ' +self.ident[0]+':'+str(self.ident[1])+' to '+self.ident[2]+':'+str(self.ident[3])
+					if c == 't':
+						print 'At ' + self.time
+					if c == 'w':
+						print 'Raw tcpdump data:'
+						print '\n'.join(self.raw_lines)
+					if c == 'a':
+						print '\nApplication-layer data:'
+						print self.appdata
+				if self.fmtdat['module']:
+					print self.fmtdat['module'].format(self)
+				if self.fmtdat['footer']:
+					print '----------------------- Match End -----------------------'
 	
 	def clear(self):
 		self.state = 0
@@ -116,11 +127,59 @@ def getlen(line):
 	lenend = minset(lenend)
 	return int(line[lenstart:lenend])
 
-try:
-	config = open(sys.argv[1]).read().split('\n')
-except:
-	print 'Usage: python tcpfind.py <config file> [<options>]\n\nConfig format:\nport:(regex|pythonfile)\n\nex:\n\t1337:(key\\{|flag\\{)\n\t5678:\\xFF\\xE4\n# asterisk (*) can be used for all ports\n\t*:globalcheck.py\n# python files will be imported as modules \n# and their "main()" function will be called with the \n# application-layer data as an argument, return \n# True or False depending on match.\n# Set the module variable "description" to give a \n# description to the check being performed.\n\nOptions:\n-d\t\tEnable debugging (super verbose) output'
-	sys.exit(0)
+
+def usage():
+	print '''tcpbread version 0.2.0
+Usage: tcpdump -i <interface> -nSv -X | python tcpbread.py [<options>] <config file> [<config file> ...]
+Tested with tcpdump versions 4.2.1 and 4.4.0.
+
+Options:
+	-d		Enable debugging (super verbose) output
+	-H		Print matches with high-visibility header
+	-F		Print matches with high-visibility footer
+	-r		Print matches with the rule they matched against
+	-w		Print matches with raw tcpdump data
+	-a		Print matches with parsed application-layer data
+	-i		Print matches with source and destination IPs and ports
+	-t		Print matches with timestamp
+	-x <pythonfile>	in addition to any of the above printing options,
+			use a script to do some formatting
+
+For the formatting options r, w, a, i, and t, if none are provided, the default is
+equivilant to "-ati". If some are provided, only those are used, and the printing
+will be done in the order they are provided.
+
+For the formatting python scripts, they will be imported as modules, and their
+format() function will be called with a Packet class as the only argument.
+Read through tcpbread.py for a description of said class. They should return a 
+string that can be printed to the screen, but they really have free reign in 
+terms of what they can do with control flow-- i.e. they could prompt the user
+for confirmation that the match is valid before submitting the metadata to 
+a malicious-connection reporting service.
+
+Config format: port:(<regex> | <pythonfile>)
+See included sample configuration file for details'''
+
+def parseconfigs(*files):
+	config = []
+	for cfile in files:
+		config += parseconfig(cfile)
+	config = [map(lambda x: x[0], config), map(lambda x: x[1], config)]
+	config[1] = map(inputswitch, config[1])
+	return config
+
+def parseconfig(cfile):
+	try:
+		config = open(cfile).read().split('\n')
+		config = map(lambda x: x[:x.index('#')] if '#' in x else x, config)
+		config = map(string.strip, config)
+		config = filter(lambda x: x != '', config)
+		config = map(lambda x: (x.split(':')[0], x.split(':')[1]), config)
+	except:
+		print 'Configuration format error in ' + cfile
+		usage()
+		sys.exit(1)
+	return config
 
 def inputswitch(line):
 	try:
@@ -134,18 +193,60 @@ def inputswitch(line):
 	except:
 		return re.compile(line)
 
-config = map(string.strip, config)
-config = filter(lambda x: x != '' and x[0] != '#', config)
-config = map(lambda x: (x.split(':')[0], x.split(':')[1]), config)
-config = [map(lambda x: x[0], config), map(lambda x: x[1], config)]
-config[1] = map(inputswitch, config[1])
+def main(config, fmtdat, debug):
+	running = Packet(config, fmtdat, debug)
+	if debug:
+		print config
+	
+	while True:
+		try:
+			line = raw_input()
+		except (Exception, KeyboardInterrupt) as e:
+			if debug:
+				print e
+			print 'Exiting.'
+			sys.exit(0)
+		if running.add_line(line):
+			running.parse()
+			running.clear()
 
-debug = '-d' in sys.argv
-running = Packet(debug)
-if debug:
-	print config
+if __name__ == '__main__':
+	import getopt
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], 'dHFraitx:', ['--help'])
+	except:
+		usage()
+		sys.exit(0)
+	fstr = ''
+	fmod = None
+	header = False
+	footer = False
+	debug = False
 
-while True:
-	if running.add_line(raw_input()):
-		running.parse()
-		running.clear()
+	for opt, arg in opts:
+		if opt == '-H':
+			header = True
+		if opt == '-F':
+			footer = True
+		if opt == '-d':
+			debug = True
+		if opt == '-x':
+			fmod = __import__(arg[:-3])
+			tmp = fmod.format
+		if opt in ('-h', '--help'):
+			usage()
+			sys.exit(0)
+		if opt in ('-r', '-w', '-a', '-i', '-t'):
+			fstr += opt[1]
+	if fstr == '':
+		fstr = 'ati'
+	if len(args) == 0:
+		usage()
+		sys.exit(1)
+	config = parseconfigs(*args)
+	fmtdat = {
+		'format': fstr,
+		'header': header,
+		'footer': footer,
+		'module': fmod }
+	main(config, fmtdat, debug)
